a25886751b61012d36f47529a460f472970abc15
Gioele <gioelebenci@live.com>
2020-02-19 23:05:09 +0000

    Update Player.hx

--- source/Player.hx
+++ source/Player.hx
@@ -2,8 +2,14 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 
 class Player extends FlxSprite {
+	var thrust:Float = 0;
+
 	public function new() {
 		// we create the sprite at the centre of the screen
 		super(FlxG.width / 2, FlxG.height / 2);
+
+		// enlarging hitbox
+		width *= 2;
+		height *= 2;
 	}
 }
