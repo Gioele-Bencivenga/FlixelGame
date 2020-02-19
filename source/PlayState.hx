e8df57388913983b51f1eaaed3022b79f82f75ff
Gioele <gioelebenci@live.com>
2020-02-19 20:15:37 +0000

    Simple menu with change of state
    Also trying to make the player appear on the screen which is not working at the moment.
    
    Also trying to import the FlxStarField to have some nice background but it seems like uncommenting the Project.xml was not enough to make me import it.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,29 @@
 package;
 
+// I don't know how to import this stuff, find out
+// https://github.com/HaxeFlixel/flixel-addons/blob/master/flixel/addons/display/FlxStarField.hx
+//import flixel.addons.display.FlxStarField.FlxStarField2D;
+import flixel.FlxG;
 import flixel.FlxState;
 
-class PlayState extends FlxState
-{
-	override public function create():Void
-	{
+class PlayState extends FlxState {
+	var player:Player;
+
+	override public function create():Void {
 		super.create();
+
+		//add(new FlxStarField2D());
+
+		player = new Player();
+		add(player);
 	}
 
-	override public function update(elapsed:Float):Void
-	{
+	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
+
+		// pressing ESC brings back to the menu
+		if (FlxG.keys.pressed.ESCAPE) {
+			FlxG.switchState(new MenuState());
+		}
 	}
 }
---
+++ source/Player.hx
@@ -0,0 +1,9 @@
+import flixel.FlxG;
+import flixel.FlxSprite;
+
+class Player extends FlxSprite{
+
+    public function new() {
+        super(FlxG.width/2, FlxG.height);
+    }
+}
